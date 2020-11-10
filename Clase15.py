f4b546be2f09f987319849fbf20dc7ca3d755dc9
Nicolas Gladkoff <ngladkoff@users.noreply.github.com>
2020-11-10 15:50:25 -0300

    Clase15

--- Clase15.py
+++ Clase15.py
@@ -1,2 +1,52 @@
 # Clase 15 - Repaso
 
+X = 'X'
+O = 'O'
+
+# Matrices
+
+def imprimir_matriz(matriz):
+    print()
+    print(matriz[0])
+    print(matriz[1])
+    print(matriz[2])
+    print()
+
+# Crear una matriz
+matriz = [[0,0,0],[0,0,0],[0,0,0]]
+
+imprimir_matriz(matriz)
+
+matriz[0][0] = X
+matriz[1][0] = X
+matriz[2][0] = X
+
+# guardar un dato en una matriz
+for fila in range(0,3):
+    for col in range(0,3):
+        if fila == col:
+            #matriz[fila][col] = X
+            pass
+
+imprimir_matriz(matriz)
+
+#leer un dato de una matriz
+for fila in range(0,len(matriz)):
+    for col in range(0, len(matriz[0])):
+        if fila == col:
+            print(matriz[fila][col])
+
+print()
+valor = matriz[1][1]
+print(valor)
+
+for fila in range(0, len(matriz)):
+    if matriz[fila][0] == matriz[fila][1] and matriz[fila][0] == matriz[fila][2] and matriz[fila][0] != 0:
+        print("Gano!!: " + matriz[fila][0])
+
+for col in range(0, len(matriz[0])):
+    if matriz[0][col] == matriz[1][col] and matriz[0][col] == matriz[2][col] and matriz[0][col] != 0:
+        print("Gano!!: "+ matriz[0][col])
+
+
+    
--- SegundoParcial.py
+++ SegundoParcial.py
@@ -1,1 +1,104 @@
 # Segundo Parcial
+
+CANTIDAD_MAQUINAS = 5
+
+class Produccion:
+    def __init__(self, maquina, hora, cantidad):
+        self.maquina= maquina
+        self.hora = hora
+        self.cantidad_producida = cantidad
+        self.cantidad_defectuosa = 0
+
+
+class Defecto:
+    def __init__(self, maquina, hora, cantidad):
+        self.maquina= maquina
+        self.hora= hora
+        self.cantidad_defectuosa = cantidad
+
+
+def main():
+    producciones = cargar_producciones()
+    defectos = cargar_defectos()
+    unificar_producciones_defectos(producciones, defectos)
+    generar_reporte_fallas_parciales(producciones, '{0} | {1} | {2} | {3} | {4:3.2f}% \n')
+    generar_reporte_maquinas_defectuosas(producciones)
+
+
+
+def cargar_producciones():
+    producciones = []
+    primera_vuelta = True
+    with open('produccion.csv', 'r') as archivo:
+        for linea in archivo:
+            if primera_vuelta:
+                primera_vuelta= False
+                continue
+            dato = linea[:-1].split(';')
+            producciones.append(Produccion(dato[0], int(dato[1]), int(dato[2])))
+    return producciones
+
+
+def cargar_defectos():
+    defectos = []
+    primera_vuelta = True
+    with open('calidad.csv', 'r') as archivo:
+        for linea in archivo:
+            if primera_vuelta:
+                primera_vuelta= False
+                continue
+            dato = linea[:-1].split(';')
+            defectos.append(Defecto(dato[0], int(dato[1]), int(dato[2])))
+    return defectos
+
+
+def unificar_producciones_defectos(producciones, defectos):
+    for defecto in defectos:
+        i = buscar_maquina_hora(defecto.maquina, defecto.hora, producciones)
+        producciones[i].cantidad_defectuosa = defecto.cantidad_defectuosa
+
+
+def buscar_maquina_hora(maquina, hora, producciones):
+    for i in range(0,len(producciones)):
+        if producciones[i].maquina == maquina and producciones[i].hora == hora:
+            return i
+    return -1
+
+
+def generar_reporte_fallas_parciales(producciones, formato):
+    with open('fparciales.txt', 'w', encoding='utf-8') as archivo:
+        for produccion in producciones:
+            porc_falla = produccion.cantidad_defectuosa / produccion.cantidad_producida
+            if porc_falla > 0.02:
+                archivo.write(formato.format(
+                                             produccion.maquina,
+                                             produccion.hora,
+                                             produccion.cantidad_producida,
+                                             produccion.cantidad_defectuosa,
+                                             porc_falla))
+
+
+def generar_reporte_maquinas_defectuosas(producciones):
+    produccion_maquina = [0,0,0,0,0]
+    defectos_maquina = [0] * CANTIDAD_MAQUINAS
+
+    for produccion in producciones:
+        i = ord(produccion.maquina) - 65
+        produccion_maquina[i] = produccion_maquina[i] + produccion.cantidad_producida
+        defectos_maquina[i] += produccion.cantidad_defectuosa
+
+    with open('fmaquinas.txt', 'w', encoding='utf-8') as archivo:
+        for i in range(0,CANTIDAD_MAQUINAS):
+            porc_falla = defectos_maquina[i] / produccion_maquina[i]
+            if porc_falla > 0.01:
+                archivo.write('{0} | {1} | {2} | {3:3.2f}% \n'.format(
+                            chr(i + 65),
+                            produccion_maquina[i],
+                            defectos_maquina[i],
+                            porc_falla
+                ))
+
+
+
+if __name__ == "__main__":
+    main()
